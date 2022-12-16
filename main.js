f1e48e998d363d4881b7cbe35ff6b24a4013fcac
amShuri <juli.meep93@gmail.com>
2022-12-16 16:34:49 -0300

    Add a prompt for the user to make their own grids

--- main.js
+++ main.js
@@ -1,15 +1,42 @@
 const body = document.body;
-const divContainer = document.createElement('div');
-divContainer.classList.add('container');
-body.appendChild(divContainer);
+
+const newGridBtn = document.createElement('button');
+newGridBtn.classList.add('new-button');
+newGridBtn.textContent = 'Generate';
+body.appendChild(newGridBtn);
+
+const container = document.createElement('div');
+container.classList.add('container');
+body.appendChild(container);
 
 for(let i = 0; i < 16; i++) {
     const childContainer = document.createElement('div');
-    childContainer.classList.add('child-contaier');
-    divContainer.appendChild(childContainer);
+    childContainer.classList.add('child-container');
+    container.appendChild(childContainer);
     for(let j = 0; j < 16; j++) {
-        const divChild = document.createElement('div');
-        divChild.classList.add('div-child')
-        childContainer.appendChild(divChild);
+        const child = document.createElement('div');
+        child.classList.add('child')
+        childContainer.appendChild(child);
     }
+    newGridBtn.addEventListener('click', () => {
+        childContainer.remove();
+    })
 }
+
+newGridBtn.addEventListener('click', () => {
+    const gridSize = prompt('Enter');
+    for(let i = 0; i < gridSize; i++) {
+        body.appendChild(container);
+        const childContainer = document.createElement('div');
+        childContainer.classList.add('child-container');
+        container.appendChild(childContainer);
+        for(let j = 0; j < gridSize; j++) {
+            const child = document.createElement('div');
+            child.classList.add('child')
+            childContainer.appendChild(child);
+        }
+        newGridBtn.addEventListener('click', () => {
+            childContainer.remove();
+        })
+    }
+})
