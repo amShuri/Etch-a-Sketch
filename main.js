1a2970d10e2200d79f535e12c417afd9ab6e1100
amShuri <juli.meep93@gmail.com>
2022-12-21 02:39:32 -0300

    Add a new button that shows/hides borders on click

--- main.js
+++ main.js
@@ -1,80 +1,35 @@
 const body = document.body;
 
+const pageTitle = document.createElement('h1');
+const btnContainer = document.createElement('div');
 const newGridBtn = document.createElement('button');
-newGridBtn.classList.add('new-grid-btn');
-newGridBtn.textContent = 'Create new grid';
-body.appendChild(newGridBtn);
-
+const hideBorderBtn = document.createElement('button');
 const flexContainer = document.createElement('div');
+
+pageTitle.textContent = 'Etch a Sketch';
+newGridBtn.textContent = 'New Grid';
+hideBorderBtn.textContent = 'Hide Borders';
+btnContainer.classList.add('btn-container');
 flexContainer.classList.add('flex-container');
+
+body.appendChild(pageTitle);
+body.appendChild(btnContainer);
+btnContainer.appendChild(newGridBtn);
+btnContainer.appendChild(hideBorderBtn);
 body.appendChild(flexContainer);
 
-createNewGrid(16)
-
-function promptInputBox() {
-    const promptFade = document.createElement('div');
-    const promptBox = document.createElement('div');
-    const promptMsg = document.createElement('p');
-    const promptInput = document.createElement('input');
-    const inputBtn = document.createElement('button');
-    const promptCloseBtn = document.createElement('div');
-    const promptCloseBox = document.createElement('div');
-
-    promptFade.classList.add('bg-fade');
-    promptBox.classList.add('prompt-box');
-    promptMsg.classList.add('prompt-msg');
-    promptInput.classList.add('prompt-input');
-    promptCloseBox.classList.add('prompt-close-box');
-    promptCloseBtn.classList.add('prompt-close-btn');
-
-    promptInput.setAttribute('type', 'number');
-    promptInput.setAttribute('placeholder', '1 ~ 100');
-    promptInput.setAttribute('min', '1');
-    promptInput.setAttribute('max', '100');
-    inputBtn.textContent = 'Create';
-    promptMsg.textContent = 'Enter a number between 1 and 100 to create a new grid';
-/*     promptCloseBtn.textContent = 'x' */
-
-    body.appendChild(promptFade);
-    promptFade.appendChild(promptBox);
-    promptBox.appendChild(promptCloseBox);
-    promptCloseBox.appendChild(promptCloseBtn)
-    promptBox.appendChild(promptMsg);
-    promptBox.appendChild(promptInput);
-    promptBox.appendChild(inputBtn);
-
-    promptInput.focus();
-
-    inputBtn.addEventListener('click', () => {
-        const flexDivs = document.querySelectorAll('div');
-        if (promptInput.value >= 1 && promptInput.value <= 100) {
-            for(const flexDiv of flexDivs) {
-                if(flexDiv.classList.contains('flex-child')) {
-                    flexDiv.remove();
-                }
-            }
-            promptBox.remove();
-            promptFade.remove();
-            createNewGrid(promptInput.value);
+hideBorderBtn.addEventListener('click', () => {
+    const flexDivs = document.querySelectorAll('div');
+    for(const flexDiv of flexDivs) {
+        if (flexDiv.style.borderWidth !== '0px') {
+            flexDiv.style.borderWidth = '0px';
+            hideBorderBtn.textContent = 'Show Borders';
         } else {
-            promptMsg.textContent = 'Input not valid! Enter a numeric value between 1 and 100'
-            promptMsg.classList.add('warning-color');
-            promptInput.value = '';
-            promptInput.focus();
-        } 
-    })
-    
-    promptInput.addEventListener('keypress', (e) => {
-        if(e.key === 'Enter') {
-            inputBtn.click();
+            flexDiv.style.borderWidth = '1px';
+            hideBorderBtn.textContent = 'Hide Borders';
         }
-    })
-
-    promptCloseBtn.addEventListener('click', () => {
-        promptBox.remove();
-        promptFade.remove();
-    })
-}
+    } 
+})
 
 function createNewGrid(gridSize) {
     for(let i = 0; i < gridSize; i++) {
@@ -83,7 +38,7 @@ function createNewGrid(gridSize) {
         flexContainer.appendChild(flexChild);
         for(let j = 0; j < gridSize; j++) {
             const flexItem = document.createElement('div');
-            flexItem.classList.add('flex-item')
+            flexItem.classList.add('flex-item');
             flexChild.appendChild(flexItem);
             flexItem.addEventListener('mouseenter', () => {
                 flexItem.style.backgroundColor = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`;
@@ -108,4 +63,69 @@ function randomColor() {
     return Math.floor(Math.random() * 256);
 }
 
+function promptInputBox() {
+    const promptFade = document.createElement('div');
+    const promptBox = document.createElement('div');
+    const promptMsg = document.createElement('p');
+    const promptInput = document.createElement('input');
+    const inputBtn = document.createElement('button');
+    const promptCloseBtn = document.createElement('div');
+    const promptCloseBox = document.createElement('div');
+
+    inputBtn.textContent = 'Create';
+    promptMsg.textContent = 'Enter a number between 1 and 100 to create a new grid';
+    promptFade.classList.add('bg-fade');
+    promptBox.classList.add('prompt-box');
+    promptMsg.classList.add('prompt-msg');
+    promptInput.classList.add('prompt-input');
+    promptCloseBox.classList.add('prompt-close-box');
+    promptCloseBtn.classList.add('prompt-close-btn');
+
+    promptInput.setAttribute('type', 'number');
+    promptInput.setAttribute('placeholder', '1 ~ 100');
+    promptInput.setAttribute('min', '1');
+    promptInput.setAttribute('max', '100');
+
+    body.appendChild(promptFade);
+    promptFade.appendChild(promptBox);
+    promptBox.appendChild(promptCloseBox);
+    promptCloseBox.appendChild(promptCloseBtn);
+    promptBox.appendChild(promptMsg);
+    promptBox.appendChild(promptInput);
+    promptBox.appendChild(inputBtn);
+
+    promptInput.focus();
+
+    inputBtn.addEventListener('click', () => {
+        const flexDivs = document.querySelectorAll('div');
+        if (promptInput.value >= 1 && promptInput.value <= 100) {
+            for(const flexDiv of flexDivs) {
+                if(flexDiv.classList.contains('flex-child')) {
+                    flexDiv.remove();
+                }
+            }
+            promptBox.remove();
+            promptFade.remove();
+            createNewGrid(promptInput.value);
+        } else {
+            promptMsg.textContent = 'Input not valid! Enter a numeric value between 1 and 100';
+            promptMsg.classList.add('prompt-warning-msg');
+            promptInput.value = '';
+            promptInput.focus();
+        } 
+    })
+
+    promptInput.addEventListener('keypress', (e) => {
+        if(e.key === 'Enter') {
+            inputBtn.click();
+        }
+    })
+
+    promptCloseBtn.addEventListener('click', () => {
+        promptBox.remove();
+        promptFade.remove();
+    })
+}
+
+window.addEventListener('load', createNewGrid(16));
 newGridBtn.addEventListener('click', promptInputBox);
